6b45b8e6bbc9ba4359be8b243a694fd65e7aee0a
Pirooz Wallace <PiroozWallace@outlook.com>
2020-12-30 15:11:02 -0800

    basic sql connect

--- app.js
+++ app.js
@@ -1,2 +1,33 @@
 // Employee Tracker
 
+const mysql = require('mysql');
+const inquirer = require('inquirer');
+const cTable = require('console.table');
+
+var connection = mysql.createConnection({
+    host: "localhost",
+  
+    // Your port; if not 3306
+    port: 3306,
+  
+    // Your username
+    user: "root",
+  
+    // Your password
+    password: "omnicode",
+    database: "company_db"
+
+  });
+
+  connection.connect(function(err) {
+    if (err) throw err;
+    runPrompts();
+  });
+
+  function runPrompts() {
+      inquirer.prompt({
+          name: "action",
+          type: "list",
+          message: "What would you like to do?"
+      })
+  }
