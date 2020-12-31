f64c6f48677a8cdc99923a5488e4a89e2dad5d9a
Pirooz Wallace <PiroozWallace@outlook.com>
2020-12-30 17:29:11 -0800

    view/add departments

--- app.js
+++ app.js
@@ -28,6 +28,70 @@ var connection = mysql.createConnection({
       inquirer.prompt({
           name: "action",
           type: "list",
-          message: "What would you like to do?"
+          message: "What would you like to do?",
+          choices: [
+              "Add department", 
+              "Add role",
+              "Add employees",
+              "View departments",
+              "View role",
+              "View employees",
+              "Update employee roles",
+              "exit"
+          ]
+      })
+      .then(function(answer) {
+
+        switch(answer.action){
+            case "Add department":
+                addDepartment();
+                break;
+            case "View department":
+                viewDepartments();
+                break;
+        }
       })
   }
+
+  function addDepartment() {
+    inquirer.prompt([
+        {
+        type: "input",
+        name: "name",
+        message: "Enter the name of the department you're adding"
+    } ]).then(function(response) {
+        
+        connection.query("INSERT INTO department SET ?", response.name, function(err, data) {
+            if (err) throw (err);
+            
+            console.table("Department Added!");
+            runPrompts();
+        })
+    })
+}
+
+
+  function viewDepartments()
+  {
+    connection.query("SELECT * FROM department", function (err, data) {
+        console.table(data);
+    })
+}
+
+function addRole() {
+    inquirer.prompt([
+        {
+            type: "input",
+            name: "name",
+            message: "Enter role you're adding"
+        }]).then(function(response) {
+
+            connection.query("INSERT INTO role SET ?", response.name, function(err, data) {
+                if (err) throw (err);
+                console.table("Role added!");
+                runPrompts();   
+
+            })
+        })
+    ])
+}
